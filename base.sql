7fa8442572dda746834e7748717af977cf99e472
Xaphan67 <cedric.falda@gmail.com>
2023-08-30 16:51:15 +0200

    Mise à jour : Base de données

--- base.sql
+++ base.sql
@@ -41,27 +41,26 @@ CREATE TABLE IF NOT EXISTS `message` (
   PRIMARY KEY (`id_message`),
   KEY `message_fk1` (`sujet_id`),
   KEY `message_fk2` (`visiteur_id`),
-  CONSTRAINT `message_fk1` FOREIGN KEY (`sujet_id`) REFERENCES `sujet` (`id_sujet`),
+  CONSTRAINT `message_fk1` FOREIGN KEY (`sujet_id`) REFERENCES `sujet` (`id_sujet`) ON DELETE CASCADE,
   CONSTRAINT `message_fk2` FOREIGN KEY (`visiteur_id`) REFERENCES `visiteur` (`id_visiteur`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table forum.message : ~7 rows (environ)
 INSERT INTO `message` (`id_message`, `texteMessage`, `dateCreationMessage`, `visiteur_id`, `sujet_id`) VALUES
-	(1, 'Texte du message...', '2023-08-29 11:39:03', 1, 1),
-	(2, 'Test ajout message', '2023-08-29 15:04:43', 1, 1),
-	(3, 'Test 2', '2023-08-29 15:10:33', 1, 1),
-	(4, 'test 3', '2023-08-29 15:13:05', 1, 1),
-	(5, 'Aliev', '2023-08-29 15:13:22', 1, 1),
-	(6, 'gvygvgigbiyu', '2023-08-29 15:14:30', 1, 1),
-	(7, 'test du message', '2023-08-29 16:09:36', 1, 5),
-	(8, 'En fait, pas si bien', '2023-08-29 16:47:39', 1, 6);
+	(3, 'Test en tant que cédric', '2023-08-30 16:36:15', 3, 5),
+	(4, 'Pas mal, je peux même me répondre à moi-même !', '2023-08-30 16:37:59', 3, 5),
+	(5, 'Juste pour... tester', '2023-08-30 16:44:26', 3, 6),
+	(6, 'Je réponds au mon propre message', '2023-08-30 16:44:43', 3, 6),
+	(7, 'Ah, on peut pas éditer.', '2023-08-30 16:44:59', 3, 6),
+	(10, ':)', '2023-08-30 16:46:14', 3, 9),
+	(11, 'Je réponds avec un autre utilisateur', '2023-08-30 16:47:32', 4, 9);
 
 -- Listage de la structure de table forum. sujet
 CREATE TABLE IF NOT EXISTS `sujet` (
   `id_sujet` int NOT NULL AUTO_INCREMENT,
   `titreSujet` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `dateCreationSujet` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `verouilleSujet` binary(50) NOT NULL DEFAULT '0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
+  `verouilleSujet` tinyint(1) NOT NULL DEFAULT '0',
   `visiteur_id` int NOT NULL,
   `categorie_id` int NOT NULL,
   PRIMARY KEY (`id_sujet`),
@@ -69,13 +68,13 @@ CREATE TABLE IF NOT EXISTS `sujet` (
   KEY `sujet_fk2` (`categorie_id`),
   CONSTRAINT `sujet_fk1` FOREIGN KEY (`visiteur_id`) REFERENCES `visiteur` (`id_visiteur`),
   CONSTRAINT `sujet_fk2` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table forum.sujet : ~2 rows (environ)
+-- Listage des données de la table forum.sujet : ~3 rows (environ)
 INSERT INTO `sujet` (`id_sujet`, `titreSujet`, `dateCreationSujet`, `verouilleSujet`, `visiteur_id`, `categorie_id`) VALUES
-	(1, 'Sujet1', '2023-08-28 16:12:40', _binary 0x66616c7365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 1, 1),
-	(5, 'Test nom sujet 2', '2023-08-29 16:09:36', _binary 0x3000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 1, 1),
-	(6, 'Nouvelle discution trop bien !', '2023-08-29 16:47:39', _binary 0x3000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, 1, 2);
+	(5, 'test', '2023-08-30 16:36:15', 1, 3, 1),
+	(6, 'Autre test', '2023-08-30 16:44:25', 0, 3, 1),
+	(9, 'Une autre catégorie !', '2023-08-30 16:46:14', 0, 3, 2);
 
 -- Listage de la structure de table forum. visiteur
 CREATE TABLE IF NOT EXISTS `visiteur` (
@@ -84,13 +83,16 @@ CREATE TABLE IF NOT EXISTS `visiteur` (
   `mdpVisiteur` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `dateInscriptionVisiteur` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `emailVisiteur` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `roleVisiteur` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id_visiteur`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `roleVisiteur` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'ROLE_MEMBER',
+  PRIMARY KEY (`id_visiteur`) USING BTREE,
+  UNIQUE KEY `pseudoVisiteur` (`pseudoVisiteur`),
+  UNIQUE KEY `emailVisiteur` (`emailVisiteur`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table forum.visiteur : ~1 rows (environ)
+-- Listage des données de la table forum.visiteur : ~2 rows (environ)
 INSERT INTO `visiteur` (`id_visiteur`, `pseudoVisiteur`, `mdpVisiteur`, `dateInscriptionVisiteur`, `emailVisiteur`, `roleVisiteur`) VALUES
-	(1, 'Pseudo1', '1234', '2023-08-28 16:11:55', 'test@visiteur.fr', 'membre');
+	(3, 'Cedric', '$2y$10$0wFOMcDZPiuoPpUcKRGbP.w25ArEZSndE9vuGiX23bA.wyDL72a86', '2023-08-30 16:17:38', 'cedric.falda@gmail.com', 'Membre'),
+	(4, 'Aliev', '$2y$10$9iKWfSdOxCKSPlwMGS4pLu4RnRXGIs1ttwKinRqWdT0LRKlI/eeMG', '2023-08-30 16:46:50', 'aliev@jesaispas.com', 'ROLE_MEMBER');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
