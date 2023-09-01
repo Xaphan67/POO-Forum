6c64f14cc53cb6cd0d657ee4328170913a9ef07c
Xaphan67 <cedric.falda@gmail.com>
2023-09-01 09:01:16 +0200

    Ajout : Messages d'erreur lors de l'inscription / connexion

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -27,21 +27,39 @@ class SecurityController extends AbstractController implements ControllerInterfa
                 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL, FILTER_VALIDATE_EMAIL);
                 $mdp = filter_input(INPUT_POST, "mdp", FILTER_SANITIZE_SPECIAL_CHARS);
                 $mdpCheck = filter_input(INPUT_POST, "mdpCheck", FILTER_SANITIZE_SPECIAL_CHARS);
-                if ($pseudo && $email && $mdp && $mdpCheck)
+                if ($pseudo && $email && $mdp && $mdpCheck && !$visitorManager->findOneByEmail($email) && !$visitorManager->findOneByPseudo($pseudo) && ($mdp == $mdpCheck))
                 {
-                    if (!$visitorManager->findOneByEmail($email)) // Vérifie que l'email n'existe pas
-                    {
-                        if (!$visitorManager->findOneByPseudo($pseudo)) // Vérifie que le pseudo n'existe pas
-                        {
-                            if ($mdp == $mdpCheck) { // Vérifie que mdp et mdpCheck sont identiques
-                                $visitorManager->add(['pseudoVisiteur' => $pseudo , 'mdpVisiteur' => password_hash($mdp, PASSWORD_DEFAULT), 'emailVisiteur' => $email]); // Hashe le mdp et ajoute les informations du formulaire en BDD
-                                Session::addFlash("success", 'Merci ! Vous êtes désormais inscrit en tant que "$pseudo"');
-                            }
-                        }
-                    }    
+                    $visitorManager->add(['pseudoVisiteur' => $pseudo , 'mdpVisiteur' => password_hash($mdp, PASSWORD_DEFAULT), 'emailVisiteur' => $email]); // Hashe le mdp et ajoute les informations du formulaire en BDD
+                    Session::addFlash("success", 'Merci ! Vous êtes désormais inscrit en tant que "$pseudo"');
+                    $this->redirectTo("forum", "listCategories"); // Redirige vers la liste des catégories
                 }
+                switch (true) { // Affiche une erreur via un message en fonction du probleme
+                    case !$pseudo:
+                        Session::addFlash("error", "Le pseudonyme est invalide !");
+                        break;
+                    case !$email:
+                        Session::addFlash("error", "L'adresse e-mail est invalide !");
+                        break;
+                    case !$mdp:
+                        Session::addFlash("error", "Le mot de passe est invalide !");
+                        break;
+                    case !$mdpCheck:
+                        Session::addFlash("error", "Le mot de passe est invalide !");
+                        break;
+                    case $visitorManager->findOneByEmail($email):
+                        Session::addFlash("error", "Cette adresse e-mail est déjà utilisée !");
+                        break;
+                    case $visitorManager->findOneByPseudo($pseudo):
+                        Session::addFlash("error", "Ce pseudonyme est déjà utilisé !");
+                        break;
+                    case $mdp == $mdpCheck:
+                        Session::addFlash("error", "Les mots de passe ne correspondent pas !");
+                        break;
+                }
+                $this->redirectTo("security", "register"); // Redirige vers le formulaire d'inscription
             }
-            $this->redirectTo("forum", "listCategories"); // Redirige vers la liste des catégories
+            Session::addFlash("error", "Au moins un champ du formulaire est invalide !");
+            $this->redirectTo("security", "register"); // Redirige vers le formulaire d'inscription
         } else {
             return [
                 "view" => VIEW_DIR . "register.php"
@@ -57,13 +75,27 @@ class SecurityController extends AbstractController implements ControllerInterfa
                 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL, FILTER_VALIDATE_EMAIL);
                 $mdp = filter_input(INPUT_POST, "mdp", FILTER_SANITIZE_SPECIAL_CHARS);
                 $passwordHash = $visitorManager->getPasswordHash($email)["mdpVisiteur"];
-                if (password_verify($mdp, $passwordHash)) { // Vérifie que le mdp saisi est valide
+                if ($email && $mdp && password_verify($mdp, $passwordHash)) { // Vérifie les champs et que le mdp saisi est valide
                     $user = $visitorManager->findOneByEmail($email);
                     Session::setUser($user);
                     Session::addFlash("success", "Bienvenue " . $user . " !");
+                    $this->redirectTo("forum", "listCategories"); // Redirige vers la liste des catégories
                 }
-                $this->redirectTo("forum", "listCategories"); // Redirige vers la liste des catégories
+                switch (true) { // Affiche une erreur via un message en fonction du probleme
+                    case !$email:
+                        Session::addFlash("error", "L'adresse e-mail est invalide !");
+                        break;
+                    case !$mdp:
+                        Session::addFlash("error", "Le mot de passe est invalide !");
+                        break;
+                    case !password_verify($mdp, $passwordHash):
+                        Session::addFlash("error", "Le mot de passe est incorect !");
+                        break;
+                }
+                $this->redirectTo("security", "login"); // Redirige vers la liste des catégories
             }
+            Session::addFlash("error", "Au moins un champ du formulaire est invalide !");
+            $this->redirectTo("security", "login"); // Redirige vers la liste des catégories
         } else {
             return [
                 "view" => VIEW_DIR . "login.php"
