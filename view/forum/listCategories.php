962b7b68f1fbd1c5b595e2fe3d1824445cd1b104
Xaphan67 <cedric.falda@gmail.com>
2023-09-11 10:57:13 +0200

    Ajout boutons d'action vue Liste Catégories

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -48,8 +48,6 @@ $categories = $result["data"]['categories'];
                                             </form>
                                             <button onclick="showEditForm(<?= $category->getId() ?>)" type="submit" name="cancel">Annuler</button>
                                         </div>
-                                        <button id="editBtn<?= $category->getID() ?>" onclick="showEditForm(<?= $category->getId() ?>)" type="submit" name="edit">Modifier</button>
-                                        <a href="index.php?ctrl=categorie&action=deleteCategory&id=<?= $category->getID() ?>">Supprimer</a>
                                     <?php
                                     }
                                     ?>
@@ -63,12 +61,33 @@ $categories = $result["data"]['categories'];
                                                 <button type="submit" name="editDesc">Valider</button>
                                             </form>
                                             <button onclick="showEditDescForm(<?= $category->getId() ?>)" type="submit" name="cancel">Annuler</button>
-                                        </div>
-                                        <button id="editDescBtn<?= $category->getID() ?>" onclick="showEditDescForm(<?= $category->getId() ?>)" type="submit" name="edit"><?= empty($category->getDescriptionCategorie()) ? "Ajouter une description" : "Modifier" ?></button>
+                                    </div>
                                     <?php
                                     }
                                     ?>
                                 </div>
+                                <?php if (App\Session::getUser() && App\Session::isAdmin()) {
+                                ?>
+                                <div class="actions">
+                                    <button class="btn" id="editBtn<?= $category->getID() ?>" onclick="showEditForm(<?= $category->getId() ?>)" type="submit" name="edit" title="Modifier le nom de la catégorie">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.9em" viewBox="0 0 448 512">
+                                            <path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+                                        </svg>
+                                    </button>
+                                    <button class="btn" id="editDescBtn<?= $category->getID() ?>" onclick="showEditDescForm(<?= $category->getId() ?>)" type="submit" name="edit" title="Modifier la description de la catégorie">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.7em" viewBox="0 0 512 512">
+                                            <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
+                                        </svg>
+                                    </button>
+                                    <a class="btn" href="index.php?ctrl=categorie&action=deleteCategory&id=<?= $category->getID() ?>" title="Supprimer la catégorie">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
+                                            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <?php
+                                }
+                                ?>
                             </div>
                         </td>
                         <td class="cellCenter"><?= $category->getNbSujets() ?></td> <!-- Nombre de sujets de la catégorie -->
